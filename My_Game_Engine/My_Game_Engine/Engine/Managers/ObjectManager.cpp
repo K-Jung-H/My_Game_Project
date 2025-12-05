c784a5fab1585c1d6695978cf4d2e900aad9b5b6
KJH <jay5136@naver.com>
2025-12-05 15:42:34 +0900

    문제 해결 및 테스트 완료

--- My_Game_Engine/My_Game_Engine/Engine/Managers/ObjectManager.cpp
+++ My_Game_Engine/My_Game_Engine/Engine/Managers/ObjectManager.cpp
@@ -136,8 +136,14 @@ void ObjectManager::DestroyObjectRecursive(Object* pObject)
 
     m_pOwnerScene->UnregisterAllComponents(pObject);
 
+
+    auto it = m_NameToObjectMap.find(pObject->GetName());
+    if (it != m_NameToObjectMap.end() && it->second == pObject)
+    {
+        m_NameToObjectMap.erase(it);
+    }
+
     UINT id = pObject->GetId();
-    m_NameToObjectMap.erase(pObject->GetName());
     m_ActiveObjects.erase(id);
     ReleaseId(id);
 }
@@ -199,12 +205,36 @@ void ObjectManager::ReleaseId(UINT id)
     m_FreeList.push(id);
 }
 
-void ObjectManager::SetObjectName(Object* pObject, const std::string& newName) 
+void ObjectManager::SetObjectName(Object* pObject, const std::string& newName)
 {
     if (!pObject) return;
-    m_NameToObjectMap.erase(pObject->GetName());
-    pObject->SetName(newName);
-    m_NameToObjectMap[newName] = pObject;
+    if (pObject->GetName() == newName) return;
+
+    std::string validName = newName;
+    if (validName.empty())
+    {
+        validName = "Object";
+    }
+
+    if (m_NameToObjectMap.find(validName) != m_NameToObjectMap.end())
+    {
+        std::string uniqueName = validName;
+        int counter = 1;
+        while (m_NameToObjectMap.find(uniqueName) != m_NameToObjectMap.end())
+        {
+            uniqueName = validName + "_" + std::to_string(counter++);
+        }
+        validName = uniqueName;
+    }
+
+    auto oldIt = m_NameToObjectMap.find(pObject->GetName());
+    if (oldIt != m_NameToObjectMap.end() && oldIt->second == pObject)
+    {
+        m_NameToObjectMap.erase(oldIt);
+    }
+
+    pObject->SetName(validName);
+    m_NameToObjectMap[validName] = pObject;
 }
 
 void ObjectManager::SetParent(Object* pChild, Object* pNewParent) 
